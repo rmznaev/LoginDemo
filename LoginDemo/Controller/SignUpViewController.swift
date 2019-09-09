828e1e10abb4dfdfaa58a9e22e4e556ddc813280
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-09-10 01:52:35 +0400

    MVC pattern & storyboards are designed

---
+++ LoginDemo/Controller/SignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpViewController.swift
+//  LoginDemo
+//
+//  Created by Ramazan Abdullayev on 9/10/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
